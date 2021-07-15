0730c35163551e057a4a5c46e0cf79da2206c490
w3bs <w3bs.utils@gmail.com>
2021-07-15 05:40:50 +0200

    Add files via upload

---
+++ Bootstrap.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Template\SnackysChildMin;
+
+class Bootstrap extends \Template\Snackys\Bootstrap
+{
+    public function boot(): void
+    {
+        parent::boot();
+        // whatever you do, always call parent::boot() or delete this method!
+    }
+
+    protected function registerPlugins(): void
+    {
+        parent::registerPlugins();
+        // whatever you do, always call parent::registerPlugins() or delete this method!
+    }
+}
